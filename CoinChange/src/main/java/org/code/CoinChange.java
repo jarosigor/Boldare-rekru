13b0e7ecb044924ea7c7df76a5ec04650529ec32
jarosigor <55409506+jarosigor@users.noreply.github.com>
2023-07-06 21:36:57 +0200

    Fixed errors with division

--- CoinChange/src/main/java/org/code/CoinChange.java
+++ CoinChange/src/main/java/org/code/CoinChange.java
@@ -1,31 +1,38 @@
 package org.code;
 
-import java.lang.invoke.LambdaMetafactory;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class CoinChange {
-
     public static void getChange(CoinInventory bank, double change) {
         double changeLeft = change;
+        Map<Coin, Integer> potentialChange = new TreeMap<>(Comparator
+                .comparingDouble(Coin::getDenomination).reversed());
 
         for (Coin coin : bank.getInventory().keySet()) {
             Double num = changeLeft / coin.getDenomination();
             int amountLeft = bank.getInventory().get(coin);
-            if (num.intValue() > 0) {
+            if (num.intValue() > 0 && amountLeft != 0) {
                 if (num.intValue() <= amountLeft) {
-                    changeLeft -= coin.getDenomination() * num;
-                    bank.takeCoin(coin, num.intValue());
-                    System.out.println("Wydaj " + num.intValue() + " monet "
-                            + coin.getDenomination() + coin.getCurrency());
+                    changeLeft -= coin.getDenomination() * num.intValue();
+                    potentialChange.put(coin, num.intValue());
                 } else {
                     changeLeft -= coin.getDenomination() * amountLeft;
-                    bank.takeCoin(coin, amountLeft);
-                    System.out.println("Wydaj " + amountLeft + " monet "
-                            + coin.getDenomination() + coin.getCurrency());
+                    potentialChange.put(coin, amountLeft);
                 }
+                changeLeft = BigDecimal.valueOf(changeLeft).setScale(2, RoundingMode.HALF_UP).doubleValue();
             }
 
             if (changeLeft == 0.0) {
+                for (Map.Entry<Coin, Integer> coinEntry : potentialChange.entrySet()) {
+                    bank.takeCoin(coinEntry.getKey(), coinEntry.getValue());
+                    System.out.println("Wydaj " + coinEntry.getValue() + " monet "
+                            + coinEntry.getKey().getDenomination() + coinEntry.getKey().getCurrency());
+                }
                 System.out.println("Udalo sie wydac reszte");
                 return;
             }
@@ -60,6 +67,7 @@ public class CoinChange {
         while (bank.getCoinsQuantity() > 0) {
             System.out.println("Podaj reszte: ");
             change = scanner.nextDouble();
+            System.out.println(change);
             getChange(bank, change);
 
             if (bank.getCoinsQuantity() <= 0) {
